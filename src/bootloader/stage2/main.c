059087c8aea867167ced966861232180efb2bfe8
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-05 19:19:19 +0800

    Replace infinite loop with halt instruction in Stage 2 bootloader

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -5,5 +5,5 @@ void _cdecl cstart_(uint16_t bootDrive) {
     if(bootDrive) {}
     puts("================================================================================");
     puts("Stage 2 bootloader loaded successfully!\r\n");
-    for (;;);
+    while(1) { __asm { hlt } }
 }
