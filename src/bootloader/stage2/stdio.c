41191ac8f2ea512e536f30aafaafb0baf0ba8cc3
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-05 18:18:02 +0800

    Add .clang-format file and refactor code style in bootloader files

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -1,16 +1,21 @@
 #include "stdio.h"
+
 #include "x86.h"
 
-void putc(char c)
-{
-    x86_Video_WriteCharTeletype(c, 0);
-}
+void putc(char c) { x86_Video_WriteCharTeletype(c, 0); }
 
-void puts(const char* str)
-{
-    while(*str)
-    {
+void puts(const char* str) {
+    while (*str) {
         putc(*str);
         str++;
+    }
+}
+
+#define PRINTF_NORMAL_STATE 0
+
+void _cdecl printf(const char* fmt, ...) {
+    int state = PRINTF_NORMAL_STATE;
+    while (*fmt) {
+        fmt++;
     }
 }
