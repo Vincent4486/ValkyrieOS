ea185e5e3b90f1bc35be65ba216098a58c805ea2
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-05 11:52:58 +0800

    Define entry section and set up stack in main.asm

--- src/bootloader/stage2/main.asm
+++ src/bootloader/stage2/main.asm
@@ -1,3 +1,16 @@
 bits 16
 
-section _ENTRY
+section _ENTRY class=CODE
+
+extern _cstart_
+
+global entry
+
+entry:
+    cli
+    mov ax, ds
+    mov ss, ax
+    mov sp, 0
+    mov bp, sp
+    sti
+    
