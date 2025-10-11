5b5d2547ddffa9623659423f44c7fa3d5fccca55
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-11 18:12:00 +0800

    Refactor build scripts

--- buildscripts/toolchain.mk
+++ buildscripts/toolchain.mk
@@ -1,30 +1,35 @@
+TOOLCHAIN_PREFIX=$(abspath toolchain/$(TARGET))
+export PATH := $(TOOLCHAIN_PREFIX)/bin:$(PATH)
+export TARGET_CC=$(TARGET)-gcc
+export TARGET_CXX=$(TARGET)-g++
+export TARGET_LD=$(TARGET)-ld
+
 toolchain: toolchain_binutils toolchain_gcc
 
-BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-2.45.tar.xz
-GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz
-TOOLCHAIN_PREFIX=toolchain/i686-elf
-TARGET=i686-elf
 MAKE=make
+
+BINUTILS_BUILD_DIR=toolchain/binutils-build-$(BINUTILS_VERSION)
+GCC_BUILD_DIR=toolchain/gcc-build-$(GCC_VERSION)
 
 toolchain_binutils:
 	mkdir toolchain 
 	cd toolchain && wget $(BINUTILS_URL)
-	cd toolchain && tar -xf binutils-2.45.tar.xz
-	mkdir toolchain/binutils-build-2.45
-	cd toolchain/binutils-build-2.45 && ../binutils-2.45/conf \
+	cd toolchain && tar -xf binutils-$(BINUTILS_VERSION).tar.xz
+	mkdir $(BINUTILS_BUILD_DIR)
+	cd $(BINUTILS_BUILD_DIR) && ../binutils-$(BINUTILS_VERSION)/conf \
 		--prefix="$(TOOLCHAIN_PREFIX)"	\
 		--target=$TARGET				\
 		--with-sysroot					\
 		--disable-nls					\
 		--deiable-werror
-	$(MAKE) -j4 -C toolchain/binutils-build-2.45
-	$(MAKE) -C toolchain/binutils-build-2.45 install
+	$(MAKE) -j4 -C $(BINUTILS_BUILD_DIR)
+	$(MAKE) -C $(BINUTILS_BUILD_DIR) install
 
 toolchain_gcc: toolchain_binutils
 	cd toolchain && wget $(GCC_URL)
-	cd toolchain && tar -xf gcc-11.2.0.tar.xz
-	mkdir toolchain/gcc-build-11.2.0
-	cd toolchain/gcc-build-11.2.0 && ../binutils-2.45/conf \
+	cd toolchain && tar -xf gcc-$(GCC_VERSION).tar.xz
+	mkdir $(GCC_BUILD_DIR)
+	cd $(GCC_BUILD_DIR) && ../gcc-$(GCC_VERSION)/conf \
 		--prefix="$(TOOLCHAIN_PREFIX)"	\
 		--target=$TARGET				\
 		--with-sysroot					\
@@ -32,5 +37,5 @@ toolchain_gcc: toolchain_binutils
 		--endable-language=c,c++		\
 		--without-headers				\
 
-	$(MAKE) -j4 -C toolchain/gcc-build-11.2.0 all-gcc all-target-libgcc
-	$(MAKE) -C toolchain/gcc-build-11.2.0 install-gcc install-target-libgcc
+	$(MAKE) -j4 -C $(GCC_BUILD_DIR) all-gcc all-target-libgcc
+	$(MAKE) -C $(GCC_BUILD_DIR) install-gcc install-target-libgcc
