5d4f7cb588ce602714d02e9c2171b14c79939643
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-11 19:02:56 +0800

    commit

--- buildscripts/conf.mk
+++
@@ -1,7 +0,0 @@
-TARGET=i686-elf
-
-BINUTILS_VERSION=2.45
-GCC_VERSION=11.2.0
-
-BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-$(BINUTILS_VERSION).tar.xz
-GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-$(GCC_VERSION).tar.xz
