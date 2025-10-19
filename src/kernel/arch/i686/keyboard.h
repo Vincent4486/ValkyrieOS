3fdcc159cd743c77029415939264e9ab7ce697c1
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-19 10:39:19 +0800

    Enhance keyboard IRQ handling: support key releases, modifiers, and implement non-blocking readline functionality

--- src/kernel/arch/i686/keyboard.h
+++ src/kernel/arch/i686/keyboard.h
@@ -5,3 +5,5 @@
 
 void i686_keyboard_init(void);
 void i686_keyboard_irq(Registers* regs);
+int i686_keyboard_readline_nb(char *buf, int bufsize);
+int i686_keyboard_readline(char *buf, int bufsize);
