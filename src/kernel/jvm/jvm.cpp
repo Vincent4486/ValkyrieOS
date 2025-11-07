6e442845ad68ac5b7038ace1b8b7c7fa5cc3d459
Vincent Yang <yang.vincent.h@icloud.com>
2025-11-07 19:45:23 +0800

    Implement unpackJAR function and update header files for consistency

--- src/kernel/jvm/jvm.cpp
+++ src/kernel/jvm/jvm.cpp
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#include "jvm.h"
+#include "jvm.hpp"
 
 #include <std/stdio.h>
+
+void printUsage(){
+    
+}
 
 void java(const char *classPath) { 
     printf("hello\n"); 
