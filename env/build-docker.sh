752ace13d1fbb127e05e513259bb791b7edd9b45
Vincent Yang <yang.vincent.h@icloud.com>
2025-11-24 16:15:27 +0800

    Added licence identifier

--- env/build-docker.sh
+++ env/build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# SPDX Licence Identifier: AGPL v3.0 or later
+
 # Get the directory of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
